824a0c3ec5211f8ce488cb72e97fbf805759b775
j1ere <jeremywambua106@gmail.com>
2025-06-07 19:21:02 +0300

    registerview created

--- BACKEND/allprojects/settings.py
+++ BACKEND/allprojects/settings.py
@@ -138,7 +138,7 @@ from datetime import timedelta
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=5),
-    "ROTATE_REFRESH_TOKENS": False,
-    "BLACKLIST_AFTER_ROTATION": False,
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=2),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
 }
--- BACKEND/votingsys/views.py
+++ BACKEND/votingsys/views.py
@@ -1,3 +1,26 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from .serializers import RegisterSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+
+class RegisterView(APIView):
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+
+            #create JWT token manually
+            refresh = RefreshToken.for_user(user)
+
+            return Response({"message": "user registered ",
+                             "refresh": str(refresh),
+                             "access": str(refresh.access_token)},
+                              status=status.HTTP_201_CREATED
+                            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+"""
+The logic for login is already handled by default by django rest)framework_simplejwt
+"""
