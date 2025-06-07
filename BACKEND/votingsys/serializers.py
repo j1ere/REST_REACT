c336cc9439f5a1cf40b40c06826963d8471ed784
j1ere <jeremywambua106@gmail.com>
2025-06-07 18:21:29 +0300

    explanation of the create method in serializers.py

--- BACKEND/votingsys/serializers.py
+++ BACKEND/votingsys/serializers.py
@@ -17,7 +17,27 @@ class RegisterSerializer(serializers.ModelSerializer):
         return attrs
     
     def create(self, validated_data):
-        validated_data.pop('password2')
-        user = User.objects.create_user(**validated_data)
+        validated_data.pop('password2')#removing password2 from the validated_data dictionary since its only used to confirm correct password
+        user = User.objects.create_user(**validated_data)# this line unpacks the dictionary into keyword arguments 
         return user
+    #validated_data is a dictionary of cleaned and validated form data passed from a serializer
+    """
+    validated_data={
+        'username': '12345',
+        'email': 'john@example.com',
+        'password': 'pass123',
+        'password2': 'pass123'
+    }
+
+    user = User.objects.create_user(**validated_data) #this line is equivalent to : User.objects.create_user(
+    username="john123",
+    email="john@example.com",
+    password="securepassword"
+)
+
+
+you use ** when: you have a dictionary whose data matches
+ the expected parameter names of a function or method
+ and you want to pass those key-value pairs as individual keyword arguments
+    """
     
